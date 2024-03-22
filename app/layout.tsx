2a61bd3dc69559906dbad4b00c591125190aa559
Qodestackr <winchygichu@gmail.com>
2024-03-22 18:03:43 +0300

    comment header to fix later

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} `}>
-        <Headerzz />
+        {/* <Headerzz /> */}
 
         
         {children}
